5f0d994e2dad4796a345a96891408a77a04c2bda
KurnakovMaksim <maksk210@gmail.com>
2021-08-29 19:25:58 +0900

    Add specified names for GetAllClothes

--- Clothing/GetAllClothes.sql
+++ Clothing/GetAllClothes.sql
@@ -1,19 +1,18 @@
 use ClothingShop;
 
-select 
-	* 
-from Clothings;
+--select 
+--	* 
+--from Clothings;
 
 select
 	c.Name,
 	c.Price,
-	cc.Name,
-	cs.Name,
-	ct.Name,
+	cc.Name as Category,
+	cs.Name as Size,
+	ct.Name as Type,
 	i.Url
 from Clothings c
 	left join ClothingCategories cc on c.CategoryId = cc.Id
 	left join ClothingSizes cs on c.SizeId = cs.Id
 	left join ClothingTypes ct on c.TypeId = ct.Id
-	left join Images i on i.ClothingId = c.Id;
-	
+	left join Images i on c.Id = i.ClothingId;
